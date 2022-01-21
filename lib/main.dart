1bd496cbc1067396a4ce837fe9cbf852a74fdcc4
Jayesh Mardiya <mardiyajayesh@gmail.com>
2022-01-21 10:55:14 +0530

    Chore: Upgrade SDK to 0.15.0

--- lib/main.dart
+++ lib/main.dart
@@ -32,10 +32,33 @@ class _MyAppState extends State<MyApp> {
           onFinish: (context, sessionId) {},
           onError: (context, error, message) {
             if (error == KvalifikaSdkError.INVALID_APP_ID) {
-              print("invalid app id, please provide app id");
+              Text("Invalid App ID, please provide app id");
+            } else if (error == KvalifikaSdkError.USER_CANCELLED) {
+              Text("User cancelled");
+            } else if (error == KvalifikaSdkError.TIMEOUT) {
+              Text("Timeout");
+            } else if (error == KvalifikaSdkError.SESSION_UNSUCCESSFUL) {
+              Text("Session failed");
+            } else if (error == KvalifikaSdkError.ID_UNSUCCESSFUL) {
+              Text("ID scan failed");
+            } else if (error == KvalifikaSdkError.CAMERA_PERMISSION_DENIED) {
+              Text("Camera permission denied");
+            } else if (error == KvalifikaSdkError.LANDSCAPE_MODE_NOT_ALLOWED) {
+              Text("Landscape mode is not allowed");
+            } else if (error == KvalifikaSdkError.REVERSE_PORTRAIT_NOT_ALLOWED) {
+              Text("Reverse portrait is not allowed");
+            } else if (error == KvalifikaSdkError.FACE_IMAGES_UPLOAD_FAILED) {
+              Text("Could not upload face images");
+            } else if (error == KvalifikaSdkError.DOCUMENT_IMAGES_UPLOAD_FAILED) {
+              Text("Could not upload Id card or passport images");
+            } else if (error == KvalifikaSdkError.NO_MORE_ATTEMPTS) {
+              Text("No more attempt left");
+            } else if (error == KvalifikaSdkError.UNKNOWN_INTERNAL_ERROR) {
+              Text("Unknown error happened: $message");
             }
           },
-          locale: KvalifikaSdkLocale.EN,
+          locale: KvalifikaSdkLocale.GE,
+          development: true,
           builder: (context, sdk) => Center(
             child: ElevatedButton(
               onPressed: () {
